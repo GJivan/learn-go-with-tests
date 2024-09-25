9eafa1e90631c0b3c27b4d08f0ed135da93919dc
GJivan <gopal.jivan@gmail.com>
2024-09-25 10:57:11 -0700

    Updates Hello World for Paramaters

--- my-project/hello_test.go
+++ my-project/hello_test.go
@@ -1,8 +1,10 @@
 package main
 
+import "testing"
+
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
